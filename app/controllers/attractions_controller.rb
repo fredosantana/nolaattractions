8442406cb471f2ac9f547e57244214854692fc37
Wil Santana <santana.wil@gmail.com>
2016-03-22 11:25:55 +0000

    Submit function works when adding a place

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -7,4 +7,15 @@ class AttractionsController < ApplicationController
 	def new
 		@attractions = Attraction.new
 	end
+
+	def create
+		Attraction.create(attractions_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def attractions_params
+		params.require(:attraction).permit(:name, :description, :address)
+	end
 end
